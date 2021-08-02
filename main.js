a04e0851eeb8f2fdb60d16116f44b942d8f3e824
vapvin <tpehdj12@daum.net>
2021-08-02 11:05:54 +0900

    Upload

---
+++ main.js
@@ -0,0 +1,319 @@
+$(document).ready(function() {
+    // $('#fullpage').fullpage({
+    //   autoScrolling: false,
+		// fitToSection: false,
+    //   navigation: true,
+    // });
+    
+    //methods
+
+
+    var controller = new ScrollMagic.Controller();
+ 
+    var scene1 = new ScrollMagic.Scene({
+      triggerElement: "#trigger1",
+      duration: 4000,
+      triggerHook: 0,
+      offset: 0
+    })
+    .setPin("#pin1")
+    .addIndicators({name: "1 (duration: 700)"}) 
+    .addTo(controller);
+
+    var tween = TweenMax.to('#title01', 100, {
+        opacity: 0,
+    });
+
+    var scene2 = new ScrollMagic.Scene({
+        triggerElement: "#trigger1",
+        duration: 500,
+        triggerHook: 0,
+        offset: 0,
+    })
+    .setTween(tween)
+    .setClassToggle(".indi01", "active")
+    .addTo(controller)
+    .addIndicators({name: "2 (duration: 700)"});
+
+    var tween2 = TweenMax.fromTo('#title02', 100, {
+      delay: 700,
+      opacity: 0,
+  }, {
+    opacity: 1,
+    repeat: 1,
+    yoyo: true
+  });
+
+    var scene3 = new ScrollMagic.Scene({
+      triggerElement: "#trigger1",
+      duration: 1000, 
+      triggerHook: 0,
+      offset: 500,
+  })
+  .setTween(tween2)
+  .setClassToggle(".indi02", "active")
+  .addTo(controller)
+  .addIndicators({name: "3 (duration: 700)"});
+
+  var tween3 = TweenMax.fromTo('#title03', 100, {
+      opacity: 0,
+  }, {
+    opacity: 1,
+    repeat: 1,
+    yoyo: true
+  });
+
+  var scene4 = new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    duration: 1000, 
+    triggerHook: 0,
+    offset: 1500,
+})
+.setTween(tween3)
+  .setClassToggle(".indi03", "active")
+  .addTo(controller)
+.addIndicators({name: "3 (duration: 700)"});
+
+
+
+var tween4 = TweenMax.fromTo('#title04', 100, {
+  opacity: 0,
+}, {
+opacity: 1,
+repeat: 1,
+yoyo: true
+});
+
+var scene5 = new ScrollMagic.Scene({
+triggerElement: "#trigger1",
+duration: 1000, 
+triggerHook: 0,
+offset: 2500,
+})
+.setTween(tween4)
+  .setClassToggle(".indi04", "active")
+  .addTo(controller)
+.addIndicators({name: "3 (duration: 700)"});
+
+
+
+var tween5 = TweenMax.to('#title05', 100, {
+  opacity: 1,
+});
+
+var scene6 = new ScrollMagic.Scene({
+triggerElement: "#trigger1",
+duration: 500, 
+triggerHook: 0,
+offset: 3500,
+})
+.setTween(tween5)
+  .setClassToggle(".indi05", "active")
+  .addTo(controller)
+.addIndicators({name: "3 (duration: 700)"});
+
+    function effectHandler(ele){
+      $(ele).each(function (i) {
+          var bottom_of_element = $(this).offset().top + 100;
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          if (bottom_of_window > bottom_of_element) {
+            $(this).addClass('effect');
+          }else{
+            $(this).removeClass('effect');
+          }
+        });
+  }
+
+  effectHandler(".fade-effect");
+  effectHandler(".bottom-to-top");
+  effectHandler(".top-to-bottom");
+  effectHandler(".left-to-right");
+  effectHandler(".right-to-left");
+
+    $(window).scroll(function () {
+        
+        effectHandler(".fade-effect");
+        effectHandler(".bottom-to-top");
+        effectHandler(".top-to-bottom");
+        effectHandler(".left-to-right");
+        effectHandler(".right-to-left");
+    //
+  });
+
+
+  $(".pop-action").click(function(){
+      $(this).toggleClass("on");
+      $(".link-wrap").fadeToggle("on");
+  })
+
+
+  $("#interview-01").click(function(){
+    const offset = $("#interview-01-target").offset();
+    $("html, body").animate({
+      scrollTop: offset.top,
+    }, 500)
+  })
+  
+
+  $("#interview-02").click(function(){
+    const offset = $("#interview-02-target").offset();
+    $("html, body").animate({
+      scrollTop: offset.top,
+    }, 500)
+  })
+
+  $(".pop-top").click(function(){
+    $('html, body').animate({
+      scrollTop : 0
+  }, 400);
+
+return false;
+  })
+  // $(".to-inter01").click(function(){
+  //     $('#fullpage').fullpage.moveTo(9);
+  // })
+  // $(".to-inter02").click(function(){
+  //     $('#fullpage').fullpage.moveTo(10);
+  // })
+
+
+
+  const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+
+    let bgCount = 0;
+    let count = 0;
+    const prevButton = document.querySelector(".swiper-button-prev-02");
+    const nextButton = document.querySelector(".swiper-button-next-02");
+    const slideAll = document.querySelectorAll(".mySwiper02 .swiper-slide");
+
+
+    const subTitle = document.querySelectorAll(".sub-op-title");
+    const subDes = document.querySelectorAll(".sub-op-des");
+    
+    
+    const mainBG = document.querySelector(".main-05");
+
+    const tabButton = document.querySelectorAll(".tab-btns");
+    const tabItems = document.querySelectorAll(".tab-item");
+    
+    const swiper2 = new Swiper(".mySwiper02", {
+        loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next-02",
+        prevEl: ".swiper-button-prev-02",
+      },
+      
+    });
+
+    const swiper3 = new Swiper(".mySwiper03", {
+      loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next-03",
+      prevEl: ".swiper-button-prev-03",
+    },
+    
+  });
+
+
+
+  const swiper4 = new Swiper(".mySwiper4", {
+    loop: true,
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: false
+  },
+  slidesPerView: 8,
+  speed: 2000,
+  grabCursor: true,
+  mousewheelControl: true,
+  keyboardControl: true,
+
+  });
+  
+  
+  
+  
+    
+    mainBG.style.backgroundRepeat = "no-repeat";
+    mainBG.style.backgroundImage = "url('./img/sub-bg-0.png')";
+    mainBG.style.backgroundSize = "cover";
+
+    subTitle[0].classList.add("activate");
+    subDes[0].classList.add("activate");
+    
+    prevButton.addEventListener("click", function(){
+      count--;
+      bgCount = ((count % slideAll.length) + slideAll.length) % slideAll.length;
+
+      if(bgCount < 0){
+          bgCount = 1;
+      }
+      mainBG.style.backgroundImage = `url('./img/sub-bg-${bgCount}.png')`;
+
+      for(let i = 0; i < subDes.length; i++){
+          if(i == bgCount){
+              subDes[i].classList.add("activate");
+              subTitle[i].classList.add("activate");
+          } else {
+              subDes[i].classList.remove("activate");
+              subTitle[i].classList.remove("activate");
+          }
+      }
+      
+      
+    });
+
+    nextButton.addEventListener("click", function(){
+      count++;
+      bgCount = count % slideAll.length;
+      mainBG.style.backgroundImage = `url('./img/sub-bg-${bgCount}.png')`;
+
+      for(let i = 0; i < subDes.length; i++){
+          if(i == bgCount){
+              subDes[i].classList.add("activate");
+              subTitle[i].classList.add("activate");
+          } else {
+              subDes[i].classList.remove("activate");
+              subTitle[i].classList.remove("activate");
+          }
+      }
+      
+    });
+
+
+
+    for(let i = 0; i < tabButton.length; i++){
+     
+      tabButton[i].addEventListener('click', function(e){
+          
+          let curIndex = Array.from(tabButton).indexOf(e.currentTarget);
+          const curButton = document.querySelector(".tab-btns.active");
+          const curItem = document.querySelector(".tab-item.active");
+          curButton.classList.remove("active");
+          curItem.classList.remove("active");
+          e.currentTarget.classList.add("active");
+          tabItems[curIndex].classList.add("active");
+
+      })
+    }
+    
+
+
+
+});
---
+++ test.js
@@ -0,0 +1,235 @@
+function init() {
+
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 436
+        }
+    });
+
+    var tween1 = TweenMax.fromTo('#human01', 0.6, {
+        scale: 1,
+    },{
+        scale: 1.6,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween2 = TweenMax.fromTo('#human02', 0.6, {
+        scale: 1,
+    },{
+        scale: 1.6,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween3 = TweenMax.fromTo('#human03', 0.6, {
+        scale: 1,
+    },{
+        scale: 1.6,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween4 = TweenMax.to('#section-hidden01', 0.42, {
+        opacity: 0,
+    });
+
+    var tween5 = TweenMax.fromTo('#section-hidden02', 0.52, {
+        opacity: 0,
+    },{
+        opacity: 1,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween6 = TweenMax.fromTo('#section-hidden03', 0.52, {
+        opacity: 0,
+    },{
+        opacity: 1,
+        repeat: 1,
+        yoyo: true
+    });
+    var tween7 = TweenMax.fromTo('#section-hidden04', 1.52, {
+        opacity: 0,
+    },{
+        opacity: 1,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween8 = TweenMax.to('#big-circle', 0.52, {
+        scale: 0.4,
+        rotation: 360
+    });
+    var tween9 = TweenMax.to('#section-hidden05', 2.52, {
+        opacity: 1,
+    });
+
+    var tween10 = TweenMax.to('#big-circle2', 0.52, {
+        rotation: 360
+    });
+
+    var tween11 = TweenMax.fromTo('#mid-text', 0.52, {
+        opacity:0,
+    },{
+        opacity: 1,
+        repeat: 1,
+        yoyo: true
+    });
+
+    var tween12 = TweenMax.to('#section-hidden06', 0.52, {
+        opacity: 1
+    });
+    var tween13 = TweenMax.to('#finished', 0.52, {
+        y: "-200vh",
+    });
+
+
+    var scene1 = new ScrollMagic.Scene({
+        triggerElement: "#trigger1"
+
+    }).setClassToggle('.big1', 'active')
+        .setTween(tween1)
+        .addTo(controller)
+        .addIndicators({
+            name: "1"
+        });
+
+
+    var scene2 = new ScrollMagic.Scene({
+        triggerElement: "#trigger2"
+    }).setClassToggle('.big2', 'active')
+        .setTween(tween2)
+        .addTo(controller)
+        .addIndicators({
+            name: "2"
+        });
+
+    var scene3 = new ScrollMagic.Scene({
+        triggerElement: "#trigger3"
+    }).setClassToggle('.big3', 'active')
+        .setTween(tween3)
+        .addTo(controller)
+        .addIndicators({
+            name: "3"
+        });
+
+    var scene4 = new ScrollMagic.Scene({
+        triggerElement: "#trigger4"
+    })
+        .setTween(tween4)
+        .addTo(controller)
+        .addIndicators({
+            name: "4"
+        });
+
+    var scene5 = new ScrollMagic.Scene({
+        triggerElement: "#trigger5"
+    })
+        .setTween(tween5)
+        .addTo(controller)
+        .addIndicators({
+            name: "5"
+        });
+    var scene6 = new ScrollMagic.Scene({
+        triggerElement: "#trigger6"
+    })
+        .setTween(tween6)
+        .addTo(controller)
+        .addIndicators({
+            name: "6"
+        });
+    var scene7 = new ScrollMagic.Scene({
+        triggerElement: "#trigger7"
+    })
+        .setTween(tween7)
+        .addTo(controller)
+        .addIndicators({
+            name: "7"
+        });
+    var scene8 = new ScrollMagic.Scene({
+        triggerElement: "#trigger8"
+    })
+        .setTween(tween8)
+        .addTo(controller)
+        .addIndicators({
+            name: "8"
+        });
+    var scene9 = new ScrollMagic.Scene({
+        triggerElement: "#trigger9",
+        offset: -50,
+        duration: 1200
+    })
+        .setTween(tween9)
+        .addTo(controller)
+        .addIndicators({
+            name: "9"
+        });
+
+    var scene10 = new ScrollMagic.Scene({
+        triggerElement: "#trigger10",
+        offset: -50,
+        duration: 1200
+    })
+        .setTween(tween10)
+        .addTo(controller)
+        .addIndicators({
+            name: "10"
+        });
+
+    var scene11 = new ScrollMagic.Scene({
+        triggerElement: "#trigger11",
+        duration: 300
+    })
+        .setTween(tween11)
+        .addTo(controller)
+        .addIndicators({
+            name: "11"
+        });
+
+    var scene12 = new ScrollMagic.Scene({
+        triggerElement: "#trigger12",
+        offset: 500,
+        duration: 300
+    })
+        .setTween(tween12)
+        .addTo(controller)
+        .addIndicators({
+            name: "12"
+        });
+    var scene14 = new ScrollMagic.Scene({
+        triggerElement: "#trigger13",
+        offset: 500,
+    })
+        .setTween(tween13)
+        .addTo(controller)
+        .addIndicators({
+            name: "13"
+        });
+}
+var human01 = document.getElementById("human01"),
+    human02 = document.getElementById("human02"),
+    human03 = document.getElementById("human03");
+
+function toggleImg() {
+    if(human01.classList.contains("active")){
+         human01.src = "img/human01_c.png";
+    }
+
+    if(human02.classList.contains("active")){
+        human02.src = "img/human02_c.png";
+        human01.src = "img/human01.png";
+    } else {
+        human02.src = "img/human02.png";
+    }
+
+    if(human03.classList.contains("active")){
+        human03.src = "img/human03_c.png";
+        human02.src = "img/human02.png";
+    } else {
+        human03.src = "img/human03.png";
+    }
+}
+
+window.addEventListener("scroll", toggleImg);
+init()
